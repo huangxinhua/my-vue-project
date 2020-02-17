d633490656eb8613f54305c52ab465985b04965a
huangxinhua <dream.garden@163.com>
2020-02-17 20:41:27 +0800

    echarts

--- src/components/Tendency.vue
+++ src/components/Tendency.vue
@@ -14,7 +14,7 @@ export default {
   methods: {
     initData () {
       const colors = ['#5793f3', '#675bba', '#d14a61']
-      const data = {
+      const option = {
         color: colors,
         title: {
           text: '走势图',
@@ -42,38 +42,78 @@ export default {
           boundaryGap: false,
           data: this.sevenDay
         },
-        yAxis: [{
-          type: 'value',
-          name: '用户',
-          min: 0,
-          max: 200,
-          position: 'left',
-          axisLine: {
-            lineStyle: {
-              color: '#999'
+        yAxis: [
+          {
+            type: 'value',
+            name: '用户',
+            min: 0,
+            max: 200,
+            position: 'left',
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
             }
           },
-          axisLabel: {
-            formatter: '{value}'
+          {
+            type: 'value',
+            name: '订单',
+            min: 0,
+            max: 200,
+            position: 'right',
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              formatter: '{value}'
+            }
           }
-        },
-        {
-          type: 'value',
-          name: '订单',
-          min: 0,
-          max: 200,
-          position: 'right',
-          axisLine: {
-            lineStyle: {
-              color: '#999'
+        ],
+        series: [
+          {
+            name: '新注册用户',
+            type: 'line',
+            data: this.sevenDate[0],
+            yAxisIndex: 1,
+            markPoint: {
+              data: [
+                { type: 'max', name: '最大值' },
+                { type: 'min', name: '最小值' }
+              ]
             }
           },
-          axisLabel: {
-            formatter: '{value}'
+          {
+            name: '新增订单',
+            type: 'line',
+            data: this.sevenDate[1],
+            yAxisIndex: 1,
+            markPoint: {
+              data: [
+                { type: 'max', name: '最大值' },
+                { type: 'min', name: '最小值' }
+              ]
+            }
+          },
+          {
+            name: '新增管理员',
+            type: 'line',
+            data: this.sevenDate[2],
+            yAxisIndex: 1,
+            markPoint: {
+              data: [
+                { type: 'max', name: '最大值' },
+                { type: 'min', name: '最小值' }
+              ]
+            }
           }
-        }]
+        ]
       }
-      this.mychart.setOption(data)
+      this.mychart.setOption(option)
     }
   },
   watch: {
--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -19,10 +19,10 @@
           <div class="data_list all_head"><span class="data_num head">总数据：</span></div>
         </el-col>
         <el-col :span="4">
-          <div class="data_list"><span class="data_num">{{userCount}}</span>注册用户</div>
+          <div class="data_list"><span class="data_num">{{allUserCount}}</span>注册用户</div>
         </el-col>
         <el-col :span="4">
-          <div class="data_list"><span class="data_num">{{orderCount}}</span>订单</div>
+          <div class="data_list"><span class="data_num">{{allOrderCount}}</span>订单</div>
         </el-col>
       </el-row>
     </section>
@@ -41,7 +41,46 @@ export default {
   data () {
     return {
       userCount: 10,
-      orderCount: 20
+      orderCount: 20,
+      adminCount: 30,
+      allUserCount: 40,
+      allOrderCount: 60,
+      allAdminCount: 70,
+      sevenDay: [],
+      sevenDate: [[], [], []]
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+    for (let i = 6; i > -1; i--) {
+      const date = dtime(new Date().getTime() - 86400000 * i).format('YYYY-MM-DD')
+      this.sevenDay.push(date)
+      this.getSevenData()
+    }
+    this.sevenDate = [11, 22, 33]
+  },
+  methods: {
+    async getSevenData () {
+      const apiArr = [[], [], []]
+      this.sevenDay.forEach(item => {
+        apiArr[0].push(1000)
+        apiArr[1].push(200)
+        apiArr[2].push(123123)
+      })
+      const promiseArr = [...apiArr[0], ...apiArr[1], ...apiArr[2]]
+      Promise.all(promiseArr).then(res => {
+        const resArr = [[], [], []]
+        res.forEach((item, index) => {
+          if (item.status === 1) {
+            resArr[Math.floor(index / 7)].push(item.count)
+          }
+        })
+        this.sevenDate = resArr
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
